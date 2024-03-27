04c76a54b5224990f23f70566757999cdf42cd99
wavvXD! <2300864263@qq.com>
2024-03-27 21:44:16 +0800

    first commit

---
+++ alist-main/drivers/seafile/meta.go
@@ -0,0 +1,27 @@
+package seafile
+
+import (
+	"github.com/alist-org/alist/v3/internal/driver"
+	"github.com/alist-org/alist/v3/internal/op"
+)
+
+type Addition struct {
+	driver.RootPath
+
+	Address  string `json:"address" required:"true"`
+	UserName string `json:"username" required:"true"`
+	Password string `json:"password" required:"true"`
+	RepoId   string `json:"repoId" required:"false"`
+	RepoPwd  string `json:"repoPwd" required:"false"`
+}
+
+var config = driver.Config{
+	Name:        "Seafile",
+	DefaultRoot: "/",
+}
+
+func init() {
+	op.RegisterDriver(func() driver.Driver {
+		return &Seafile{}
+	})
+}
